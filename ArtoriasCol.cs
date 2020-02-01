d65783ecce0d01dc2e78333e2658894f5e8de717
Sekiro <52294366+typezero0@users.noreply.github.com>
2020-02-01 15:39:46 +0200

    Add files via upload

---
+++ ArtoriasCol.cs
@@ -0,0 +1,11 @@
+﻿//utilized only while testing
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtoriasCol : MonoBehaviour
+{
+    void OnCollisionEnter(){
+        Debug.Log("Collision Detected!");
+        }
+}
---
+++ modelCol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modelCol : MonoBehaviour
+{
+
+    private Animator anim;
+   
+    void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>(); //instantiating an Animator object
+    }
+
+    void OnTriggerEnter(Collider other){  //OnTriggerEnter includes collision with  Body too, not just Sword
+        if(other.name != "Terrain"){//ignoring Terrain collision   
+            anim.SetInteger("AnimDeath", 1);
+            Debug.Log("Collision Detected!");
+            //GetComponent<Collider>().enabled = false;
+        }
+            
+    }
+       
+}
